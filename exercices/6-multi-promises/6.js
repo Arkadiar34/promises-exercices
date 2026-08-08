6ad797d37919102029b169f39e7479e9f5d05a03
Arkadiar34 <rodrigues.gael34@gmail.com>
2026-08-08 10:31:37 +0200

    Update 6.js

--- exercices/6-multi-promises/6.js
+++ exercices/6-multi-promises/6.js
@@ -5,7 +5,7 @@
 // la 400 ms doit renvoyer Promise
 // Utilisez Promise.all() pour les combiner et combinez les résultat dans une chaine de caractère et renvoyez là
 
-// Pour lancer seulement ce test: npm run test -- 6.test.ts (attention il relance le test automatiquement à chaque modification de ce fichier)
+// Pour lancer seulement ce test: npm run test -- 6.test.js (attention il relance le test automatiquement à chaque modification de ce fichier)
 
 export default function createMultipromises() {
 	// votre code ici
