19bf0c5aa01dda033269a3b2bdab3363d7b09932
Arkadiar34 <rodrigues.gael34@gmail.com>
2026-08-08 10:31:48 +0200

    Update test command in fetchAPI documentation

--- exercices/7-fetch/7.js
+++ exercices/7-fetch/7.js
@@ -3,7 +3,7 @@
 // Trouvez comment requêter cette API avec fetch : https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 // Parmis les données que vous récupérez, retournez le titre de l'api
 
-// Pour lancer seulement ce test: npm run test -- 7.test.ts (attention il relance le test automatiquement à chaque modification de ce fichier)
+// Pour lancer seulement ce test: npm run test -- 7.test.js (attention il relance le test automatiquement à chaque modification de ce fichier)
 export default function fetchAPI() {
 	// votre code ici
 }
