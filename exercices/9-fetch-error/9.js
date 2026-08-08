f499b7b3bfcb9cf77469ace0bd31f864b93902f3
Arkadiar34 <rodrigues.gael34@gmail.com>
2026-08-08 10:32:12 +0200

    Fix test command comment for 9.js file
    Updated test command in comments to reflect correct file extension.

--- exercices/9-fetch-error/9.js
+++ exercices/9-fetch-error/9.js
@@ -2,7 +2,7 @@
 // Elle va vous envoyer des codes 400 et 500 (qui sont des erreurs), attention vous devez gérez les erreurs pour vous assurer que
 // dans les deux cas elle renvoie "Oh non une erreur !"
 
-// Pour lancer seulement ce test: npm run test -- 9.test.ts (attention il relance le test automatiquement à chaque modification de ce fichier)
+// Pour lancer seulement ce test: npm run test -- 9.test.js (attention il relance le test automatiquement à chaque modification de ce fichier)
 export default function fetchAPI() {
 	// votre code ici
 }
