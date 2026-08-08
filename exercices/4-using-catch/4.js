a4a68e107404f57ad61c5acf41d2f65c291761fd
Arkadiar34 <rodrigues.gael34@gmail.com>
2026-08-08 10:31:18 +0200

    Fix test command in comments for 4.js
    Updated test command in comments to use 4.test.js instead of 4.test.ts.

--- exercices/4-using-catch/4.js
+++ exercices/4-using-catch/4.js
@@ -1,7 +1,7 @@
 // Créez une promise qui, lorsqu'elle reçoit le mot "Artichaud" en paramètre, se rejette avec l'erreur "Le mot Artichaud est interdit". Gérez l'erreur avec `.catch()`.
 // pour le reste des mots ils devraient se résoudre en renvoyant le mot.
 
-// Pour lancer seulement ce test: npm run test -- 4.test.ts (attention il relance le test automatiquement à chaque modification de ce fichier)
+// Pour lancer seulement ce test: npm run test -- 4.test.js (attention il relance le test automatiquement à chaque modification de ce fichier)
 
 export default function createChainedPromise(mot) {
 	// votre code ici
